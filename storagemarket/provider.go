d5e2f6d8fd6549c1573c9742d32b75479d4eb7c6
xiaomdong <xiaomdong@gmail.com>
2022-12-12 10:43:46 +0800

    添加ImportDataForDeal2声明

--- storagemarket/provider.go
+++ storagemarket/provider.go
@@ -59,6 +59,8 @@ type StorageProvider interface {
 	// ImportDataForDeal manually imports data for an offline storage deal
 	ImportDataForDeal(ctx context.Context, propCid cid.Cid, data io.Reader) error
 
+	ImportDataForDeal2(ctx context.Context, propCid cid.Cid, data io.Reader, srcFile string)
+
 	// SubscribeToEvents listens for events that happen related to storage deals on a provider
 	SubscribeToEvents(subscriber ProviderSubscriber) shared.Unsubscribe
 
